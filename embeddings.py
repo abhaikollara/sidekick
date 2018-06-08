081adeaefe69507b757f672fa145e5c553b65e21
Abhai Kollara Dilip <abhai.dilip@gmail.com>
2018-06-08 19:23:21 +0530

    Bare skeleton

---
+++ embeddings.py
@@ -0,0 +1,18 @@
+
+
+class Embedding(object):
+
+    def __init__(self):
+        pass
+    
+    def load_word2vec(self, path):
+        pass
+    
+    def load_glove(self, path):
+        pass
+    
+    def get_keras_layer(trainable=False):
+        pass
+    
+    def get_pytorch_layer(trainable=False):
+        pass
