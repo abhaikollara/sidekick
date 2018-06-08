5734ffe317f5880d72c7d7c02de267c20b8fa744
Abhai Kollara Dilip <abhai.dilip@gmail.com>
2018-06-08 20:42:44 +0530

    Keras embedding works

--- embeddings.py
+++ embeddings.py
@@ -1,18 +1,43 @@
-
+import numpy as np
+from gensim.models import KeyedVectors
 
 class Embedding(object):
 
     def __init__(self):
         pass
     
-    def load_word2vec(self, path):
-        pass
-    
+    def load_word2vec(self, path, binary=True):
+        self._model = KeyedVectors.load_word2vec_format(path, binary=binary)
+        self._index2word = self._model.index2word
+        self._matrix = self._model.syn0
+        self._vocab_size, self._dim = self._matrix.shape
+
     def load_glove(self, path):
         pass
     
     def get_keras_layer(self, trainable=False):
-        pass
+        try:
+            from keras.layers.embeddings import Embedding
+        except:
+            raise ImportError('Keras not found')
+        
+        return Embedding(self._vocab_size, self._dim)
     
     def get_pytorch_layer(self, trainable=False):
         pass
+
+    @property
+    def model(self):
+        return self._model
+    
+    @property
+    def matrix(self):
+        return self._matrix
+    
+    @property
+    def vocab_size(self):
+        return self._vocab_size
+    
+    @property
+    def dim(self):
+        return self._dim
