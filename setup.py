b53abf8f2773cf271fa174795202cf541490e141
Abhai Kollara Dilip <abhai.dilip@gmail.com>
2018-06-21 17:31:48 +0530

    Set up setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='Sidekick',
+    version='0.1',
+    description='Word embeddings for me',
+    author='Abhai Kollara Dilip',
+    author_email='abhai.dilip@gmail.com',
+    license='MIT',
+    packages=find_packages())
